8e5b79a3e08aba2b1a8a30c4f979841da35c921a
Cecilia Torreira <celia.umbreon@gmail.com>
2023-04-24 10:48:26 +0100

    Fixed player movement error, checking and testing enemies speed, changed background and player picture

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -2,11 +2,14 @@ console.log("Comprobando")
 
 //* GLOBAL VARIABLES 
 const canvasDOM = document.querySelector("#canvas") //!CANVAS
-const startButtonDOM = document.querySelector(".startgame img") //! IMAGEN DE START
-const startGameDOM = document.querySelector(".startgame img") //! PANTALLA DE INICIO
-
+const startButtonDOM = document.querySelector(".startgame, .startbutton") //! IMAGEN DE START
+const startGameDOM = document.querySelector(".startgame") //! PANTALLA DE INICIO
+const gameOverDOM = document.querySelector(".gameoverbutton")
+const scoreDOM = document.querySelector("h2") //! SCORE
 const ctx = canvasDOM.getContext("2d")
 
+
+gameOverDOM.style.display="none";
 let gameObj;
 
 //* FUNCTIONS SECTIONS
@@ -21,7 +24,7 @@ startGameDOM.style.display="none";
 
 //2. Creating elements
 gameObj = new Game()  //! Class created in Game.js
-console.log("Game")
+console.log(gameObj)
 
 //3 Starting recursion
 gameObj.gameLoop()
@@ -35,4 +38,6 @@ gameObj.gameLoop()
 
 startButtonDOM.addEventListener("click", startGame) 
 
-window.addEventListener("keydown", event)  //! TERMINAR
+window.addEventListener("keydown", (event)=>{
+  gameObj.pokemon.pokemonMovement(event)
+})  //! TERMINAR
